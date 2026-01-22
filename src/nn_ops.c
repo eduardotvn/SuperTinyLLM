1f1fa405d0057a4a839abf56bf86fed9d68315f4
Eduardo Tavares <eduardo_tvn@hotmail.com>
2026-01-22 14:16:02 -0300

    Update nn_ops.c

--- src/nn_ops.c
+++ src/nn_ops.c
@@ -5,24 +5,24 @@
 
 void nn_rmsnorm(Tensor *out, Tensor *in, Tensor *weight, float eps) {
     if (in->ndim != 2) {
-        fprintf(stderr, "Error: RMSNorm needs a 2D tensor [Batch, Dim]\n");
+        fprintf(stderr, "Error: RMSNorm needs a 2D tensor [Batch, Dimension]\n");
         exit(1);
     }
 
     int rows = in->shape[0]; 
-    int dim = in->shape[1];  
+    int dimensions = in->shape[1];  
 
     for (int i = 0; i < rows; i++) {
         float ss = 0.0f;
-        for (int j = 0; j < dim; j++) {
+        for (int j = 0; j < dimensions; j++) {
             float val = tensor_get_2d(in, i, j);
             ss += val * val;
         }
 
-        ss /= dim;
+        ss /= dimensions;
         float inv_rms = 1.0f / sqrtf(ss + eps);
 
-        for (int j = 0; j < dim; j++) {
+        for (int j = 0; j < dimensions; j++) {
             float val = tensor_get_2d(in, i, j);
             float w = weight->data[j]; 
             tensor_set_2d(out, i, j, val * inv_rms * w);
@@ -32,8 +32,8 @@ void nn_rmsnorm(Tensor *out, Tensor *in, Tensor *weight, float eps) {
 
 void nn_silu(Tensor *t) {
     for (int i = 0; i < t->size; i++) {
-        float val = t->data[i];
-        float sigmoid = 1.0f / (1.0f + expf(-val));
-        t->data[i] = val * sigmoid;
+        float value = t->data[i];
+        float sigmoid = 1.0f / (1.0f + expf(-value));
+        t->data[i] = value * sigmoid;
     }
 }
